e971e1db7f935401c621878a834e20993a24f23c
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-31 23:49:52 +0530

    feat: deployment setup

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "express";
+import { UserRoutes } from "routes";
+const app = express();
+app.use(express.json());
+app.use(bodyParser());
+const port = process.env.PORT || 5000;
+app.use('/users', UserRoutes);
+app.get('/', (_, res) => res.send('Welcome to TypeScript Server'));
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
